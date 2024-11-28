58514d0eec245859ef34c0d038652bb3f46476e2
Miquel <miquelplanapinyol@gmail.com>
2024-11-28 19:00:32 +0100

    testes

--- src/test/java/planasleiman/MissionControllerTest.java
+++ src/test/java/planasleiman/MissionControllerTest.java
@@ -1,14 +1,39 @@
 package planasleiman;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import planasleiman.Missions.Mission;
 import planasleiman.Missions.MissionController;
 import planasleiman.Users.Beneficiaire;
 import planasleiman.Users.Benevole;
+import planasleiman.database.database_interface;
 
 public class MissionControllerTest {
+
+    @BeforeEach
+    public void cleanMissiontable() throws SQLException{
+        String sql = "DELETE FROM Missions";
+        Connection connection = database_interface.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.executeUpdate();
+    }
+
+    @AfterEach
+    public void cleanMission() throws SQLException{
+        String sql = "DELETE FROM Missions";
+        Connection connection = database_interface.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.executeUpdate();
+    }
+
 
 
     @Test
@@ -18,12 +43,21 @@ public class MissionControllerTest {
         assertDoesNotThrow(()->MissionController.saveMission(mission));
     }
 
+    @Test
     void couvrirMissionTest(){
         Beneficiaire benef = new Beneficiaire(1, "Cena", "Jhon", "111-111-111", "mail@mail.com", "adresse1");
         Mission mission = benef.CreateMission(1, "Mission1", "description mission 1");
+        MissionController.saveMission(mission);
         Benevole benev = new Benevole(2, "nom", "prenom", "telephone", "mail", "adresse");
         assertDoesNotThrow(()->MissionController.changeMissionBenevole(mission.getIdMission(), benev));
+    }
 
+    @Test
+    void changestatusTest(){
+        Beneficiaire benef = new Beneficiaire(1, "Cena", "Jhon", "111-111-111", "mail@mail.com", "adresse1");
+        Mission mission = benef.CreateMission(1, "Mission1", "description mission 1");
+        MissionController.saveMission(mission);
+        assertDoesNotThrow(()->MissionController.changeMissionStatus(mission.getIdMission(), "NEW_STATUS"));
     }
 
 
